eaff27ca72f1f358d55732c6190ddea16dc22e29
TobbyFranc <tobbyfranc@gmail.com>
2022-08-08 17:17:12 +0100

    js dom

---
+++ main.js
@@ -0,0 +1,9 @@
+let beginBtn = document.getElementById('fillBtn')
+let expertBtn = document.getElementById ('outlineBtn')
+
+
+
+
+beginBtn.addEventListener ('Click', (e) =>{
+    window.location.href = './begin.html';
+});
